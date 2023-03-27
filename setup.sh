ca2f33d0854d656fc651c9939144ec9e35ff7af8
Ayobami Lawal <bamiayo90@gmail.com>
2023-03-27 17:46:08 +0100

    something wrong with my terraform

--- setup.sh
+++ setup.sh
@@ -38,8 +38,8 @@ sudo apt-get install helm
 
 
 # running terraform 
-terraform init 
-terraform apply -auto-approve 
+terraform -chdir=terraform-k83/ init 
+terraform -chdir=terraform-k83/ apply -auto-approve 
 export KUBE_VAR=`terraform output --raw kubeconfig` && echo $KUBE_VAR | base64 -d -i > lke-cluster-config.yaml
 export KUBECONFIG=lke-cluster-config.yaml
 # run the 
